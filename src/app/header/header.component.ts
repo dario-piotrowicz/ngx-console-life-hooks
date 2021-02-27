ddd10830d5caec40b93ac9245e68a63c0f36e99b
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-02-27 22:10:23 +0000

    added no longer used logic from the header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,23 +1,8 @@
 import { Component } from '@angular/core';
-import { NavigationEnd, Router, RouterEvent } from '@angular/router';
-import { Observable, of } from 'rxjs';
-
-import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  public currentlyInHome: Observable<boolean[]> = of([true]);
-
-  constructor(router: Router) {
-    this.currentlyInHome = router.events.pipe(
-      filter((event: RouterEvent) => event instanceof NavigationEnd),
-      map((endEvent: NavigationEnd) => [
-        endEvent.url === '/' || endEvent.url === '',
-      ])
-    );
-  }
-}
+export class HeaderComponent {}
