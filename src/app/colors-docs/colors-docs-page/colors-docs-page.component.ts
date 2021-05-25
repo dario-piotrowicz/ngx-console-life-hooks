b9ddda7a8df112950be4179f9f0ceb8643555e89
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-05-25 20:36:53 +0100

    add colors docs page

---
+++ src/app/colors-docs/colors-docs-page/colors-docs-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-colors-docs-page',
+  templateUrl: './colors-docs-page.component.html',
+  styleUrls: ['./colors-docs-page.component.scss']
+})
+export class ColorsDocsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/colors-docs/colors-docs.module.ts
+++ src/app/colors-docs/colors-docs.module.ts
@@ -2,19 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ClearConsoleGuard } from '../clear-console.guard';
+import { ColorsDocsPageComponent } from './colors-docs-page/colors-docs-page.component';
 import { CustomComponent } from './custom/custom.component';
 import { SchemesComponent } from './schemes/schemes.component';
 import { ColorsDataViewComponent } from './colors-data-view/colors-data-view.component';
 
 @NgModule({
-  declarations: [CustomComponent, ColorsDataViewComponent, SchemesComponent],
+  declarations: [
+    CustomComponent,
+    ColorsDataViewComponent,
+    SchemesComponent,
+    ColorsDocsPageComponent,
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild([
       {
         path: '',
-        redirectTo: '/',
-        pathMatch: 'full',
+        component: ColorsDocsPageComponent,
       },
       {
         path: 'schemes',
