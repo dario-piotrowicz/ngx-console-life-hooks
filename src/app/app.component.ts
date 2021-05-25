2751040426a21ee4f3beee47317ac313364f524c
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-05-25 19:33:31 +0100

    scroll to top on routing NavigationEnd events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private router: Router, private scroller: ViewportScroller) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => this.scroller.scrollToPosition([0, 0]));
+  }
+}
